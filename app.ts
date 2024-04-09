26b88162912e3187f6eee6339e9a077729482cee
Iman Hedeshy <iman.hedeshy@gmail.com>
2024-04-09 11:41:48 -0400

    add .env files

--- app.ts
+++ app.ts
@@ -14,7 +14,7 @@ app.get('*', (_req, res) => {
   res.send('Welcome to the Express server!');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5050;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
